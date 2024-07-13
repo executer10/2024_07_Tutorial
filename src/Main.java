77144eed94cdbb2c3e51a3b363f3cbe9b7c0d8b9
executer10 <executer10@naver.com>
2024-07-14 04:35:06 +0900

    github upload 연습

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("==프로그램 시작==");
+		Scanner scanner = new Scanner(System.in);
+		String cmd = scanner.nextLine();
+		int cmd2 = scanner.nextInt();
+		System.out.println(cmd + cmd2);
+		System.out.println("==프로그램 종료==");
+		scanner.close();
+	}
+}
